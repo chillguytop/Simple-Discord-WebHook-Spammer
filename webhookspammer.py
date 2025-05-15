b9234713f883e23f71768391fab2c0fe81fc79ff
chillguytop <animaltroix@gmail.com>
2025-05-15 15:49:58 +0200

    Update webhookspammer.py

--- webhookspammer.py
+++ webhookspammer.py
@@ -4,14 +4,23 @@ Discord: !ANDY!#0001
 Github: AndyOnTop
 """
 print("""
-                                                                                                 
- _____ _           _        _ _ _     _   _____         _      _____                             
-|   __|_|_____ ___| |___   | | | |___| |_|  |  |___ ___| |_   |   __|___ ___ _____ _____ ___ ___ 
-|__   | |     | . | | -_|  | | | | -_| . |     | . | . | '_|  |__   | . | .'|     |     | -_|  _|
-|_____|_|_|_|_|  _|_|___|  |_____|___|___|__|__|___|___|_,_|  |_____|  _|__,|_|_|_|_|_|_|___|_|  
-              |_|                                                   |_|                          
-                                    Made by Andy
-                                  Github: AndyOnTop
+                                                                                            kkkkkkkk                                                
+                                                                                            k::::::k                                                
+                                                                                            k::::::k                                                
+                                                                                            k::::::k                                                
+zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz     nnnn  nnnnnnnn    uuuuuu    uuuuuu   k:::::k    kkkkkkk eeeeeeeeeeee    rrrrr   rrrrrrrrr   
+z:::::::::::::::zz:::::::::::::::zz:::::::::::::::z     n:::nn::::::::nn  u::::u    u::::u   k:::::k   k:::::kee::::::::::::ee  r::::rrr:::::::::r  
+z::::::::::::::z z::::::::::::::z z::::::::::::::z      n::::::::::::::nn u::::u    u::::u   k:::::k  k:::::ke::::::eeeee:::::eer:::::::::::::::::r 
+zzzzzzzz::::::z  zzzzzzzz::::::z  zzzzzzzz::::::z       nn:::::::::::::::nu::::u    u::::u   k:::::k k:::::ke::::::e     e:::::err::::::rrrrr::::::r
+      z::::::z         z::::::z         z::::::z          n:::::nnnn:::::nu::::u    u::::u   k::::::k:::::k e:::::::eeeee::::::e r:::::r     r:::::r
+     z::::::z         z::::::z         z::::::z           n::::n    n::::nu::::u    u::::u   k:::::::::::k  e:::::::::::::::::e  r:::::r     rrrrrrr
+    z::::::z         z::::::z         z::::::z            n::::n    n::::nu::::u    u::::u   k:::::::::::k  e::::::eeeeeeeeeee   r:::::r            
+   z::::::z         z::::::z         z::::::z             n::::n    n::::nu:::::uuuu:::::u   k::::::k:::::k e:::::::e            r:::::r            
+  z::::::zzzzzzzz  z::::::zzzzzzzz  z::::::zzzzzzzz       n::::n    n::::nu:::::::::::::::uuk::::::k k:::::ke::::::::e           r:::::r            
+ z::::::::::::::z z::::::::::::::z z::::::::::::::z       n::::n    n::::n u:::::::::::::::uk::::::k  k:::::ke::::::::eeeeeeee   r:::::r            
+z:::::::::::::::zz:::::::::::::::zz:::::::::::::::z       n::::n    n::::n  uu::::::::uu:::uk::::::k   k:::::kee:::::::::::::e   r:::::r            
+zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz       nnnnnn    nnnnnn    uuuuuuuu  uuuukkkkkkkk    kkkkkkk eeeeeeeeeeeeee   rrrrrrr            
+
 """)
 
 #imports
